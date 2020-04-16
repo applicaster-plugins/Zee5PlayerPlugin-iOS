d2bb3df7c8d3f31987f382936ac02104e2f2252e
Tejas Todkar <tejas.todkar@ltts.com>
2020-04-16 12:21:36 +0530

    AppsFlyer and Qgraph Analytic Class Created

---
+++ PluginClasses/ZEE5PlayerSDK/AnalyticsManager/AnalyticEngine+AppsFlyer.swift
@@ -0,0 +1,19 @@
+//
+//  AnalyticEngine+AppsFlyer.swift
+//  Zee5PlayerPlugin
+//
+//  Created by Tejas Todkar on 15/04/20.
+//
+
+import Foundation
+
+extension AnalyticEngine
+{
+    //MARK:- AppsFlyer Events
+    
+    
+    //MARK:- Qgraph Events.
+    
+    
+    
+}
