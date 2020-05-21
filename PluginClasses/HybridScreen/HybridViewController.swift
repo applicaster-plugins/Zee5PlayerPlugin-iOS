d48ece2e1227886a216762c44bc710b62bc2b4db
Nikolay Smorgun <ns@computools.com>
2020-05-21 17:18:54 +0300

    Update background color
    Update background color for PlayerPlugin

--- PluginClasses/HybridScreen/HybridViewController.swift
+++ PluginClasses/HybridScreen/HybridViewController.swift
@@ -119,6 +119,32 @@ class HybridViewController: UIViewController {
             self.addGradient()
             self.metadataViewContainer.isHidden = true
         }
+        let gradient:CAGradientLayer = CAGradientLayer()
+        gradient.frame.size = self.view.frame.size
+        gradient.colors = [hexStringToUIColor(hex: "#130014").cgColor,hexStringToUIColor(hex: "#2b0225").cgColor] //Or any colors
+        self.view.layer.insertSublayer(gradient, at:0)
+    }
+    
+    func hexStringToUIColor (hex:String) -> UIColor {
+        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+
+        if ((cString.count) != 6) {
+            return UIColor.gray
+        }
+
+        var rgbValue:UInt64 = 0
+        Scanner(string: cString).scanHexInt64(&rgbValue)
+
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
     }
     
     func commonInit() {
