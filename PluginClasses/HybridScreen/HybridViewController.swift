bf454cc86cbd01affdb41ad4ea41afbee54842db
sborkin <s.borkin@applicaster.com>
2020-06-28 08:31:31 +0200

    Update HybridViewController.swift

--- PluginClasses/HybridScreen/HybridViewController.swift
+++ PluginClasses/HybridScreen/HybridViewController.swift
@@ -119,7 +119,6 @@ class HybridViewController: UIViewController {
         let shareViwController = ZEE5PlayerManager.sharedInstance().currentShareViewController
         if let shareViwController = shareViwController {
             shareViwController.dismiss(animated: false)
-             ZEE5PlayerManager .sharedInstance().pause()
         }
         
         coordinator.animate(alongsideTransition: nil, completion: { (context) in
