6a006c9a63993e1f591e9ed213b8f1e669db1428
Simon Borkin <s.borkin@applicaster.com>
2020-07-22 16:45:57 +0200

    handle meta data display in case of error

--- PluginClasses/HybridScreen/HybridViewController+Subviews.swift
+++ PluginClasses/HybridScreen/HybridViewController+Subviews.swift
@@ -209,7 +209,6 @@ extension HybridViewController {
             if self.metadataViewContainer.superview != self.mainCollectionViewContainer {
                 self.mainCollectionViewContainer.addSubview(self.metadataViewContainer)
                 
-                self.metadataViewContainer.removeConstraints(self.metadataViewContainer.constraints)
                 self.metadataViewContainer.anchorToTop()
             }
         }
@@ -220,7 +219,7 @@ extension HybridViewController {
                 resetMetadataContainer()
                 return
         }
-                
+        
         let queryItems = [
             URLQueryItem(name: "type", value: "RelatedContent"),
             URLQueryItem(name: "id", value: contentId),
